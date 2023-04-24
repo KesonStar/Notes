8ed684fd7447e37a62e1e87d70b014b36f691d00
KesonStar <1183213982@qq.com>
2023-04-24 12:55:18 +0800

    Init

--- 数据结构/Homework/02.cpp
+++ 数据结构/Homework/02.cpp
@@ -18,61 +18,33 @@ int main()
 void Maxsubsequence(vector<int> &v)
 {
     int thisSum = 0, maxSum = 0;
-    int end = 0, cnt = 0;
+    int start = 0, end = 0, tmpstart = 0;
     int neg_cnt = 0;
     for (size_t i = 0; i < v.size(); i++)
     {
-        thisSum += v[i];
         if(v[i] < 0)
             neg_cnt++;
+        thisSum += v[i];
         if (thisSum < 0)
         {
             thisSum = 0;
+            tmpstart = i+1;
         }
         else if (thisSum > maxSum)
         {
-            end = i;
             maxSum = thisSum;
+            start = tmpstart;
+            end = i;
         }
     }
-
-    if (maxSum != 0)
-    {
-        int sum = maxSum;
-        for (int i = end; i >= 0; i--)
-        {
-            if (sum - v[i] != 0)
-            {
-                sum -= v[i];
-                cnt++;
-            }
-            else
-                break;
-        }
-        if(end-cnt-1>=0&&v[end-cnt-1]==0)
-            printf("%d %d %d", maxSum, 0, v[end]);
-        else
-            printf("%d %d %d", maxSum, v[end - cnt], v[end]);
-    }
-    else if (neg_cnt == v.size())
-        printf("%d %d %d", maxSum, v[0], v[v.size() - 1]);
-    else if(maxSum == 0 && neg_cnt != v.size())
-    {
-        size_t zero_pos = 0;
-        for(size_t i = 0; i < v.size(); i++)
-        {
-            if(v[i] == 0)
-            {
-                zero_pos = i;
-                break;
-            }
-        }
-        printf("%d %d %d", maxSum, v[zero_pos], v[zero_pos]);
-    }
+    if(maxSum == 0&&neg_cnt == v.size())
+        printf("%d %d %d", 0, v[0], v[v.size()-1]);
+    else if(maxSum==0&&neg_cnt!=v.size())
+        printf("%d %d %d", 0, 0, 0);
+    else
+        printf("%d %d %d", maxSum, v[start], v[end]);
 }
-
-//One WA: 
-//Input: 5
-// 0 0 1 -1 2
-// AC: 2 0 2
-// WA: 2 2 2
+/* Key:
+    the start index of the max subsequence is the first index after thisSum is set to 0
+    the end index of the max subsequence is the last index of maxSum
+*/
